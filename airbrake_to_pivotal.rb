b485de0b6bb21c79b312516db61c55b6d259503a
Jonathan Vaught <jonathan.vaught@gmail.com>
2011-10-05 13:15:45 -0400

    Cleanup crappy commented code

--- airbrake_to_pivotal.rb
+++ airbrake_to_pivotal.rb
@@ -3,15 +3,6 @@ require "./airbrake"
 require "./pivotal"
 
 class AirbrakeToPivotal < Sinatra::Base
-  # def initialize
-    # use RackEnvironment # if ENV['RACK_ENV'] == 'development'
-    
-    # raise "boom: #{ENV["AIRBRAKE_USERNAME"]}"
-
-    # @airbrake = Airbrake.new(ENV["AIRBRAKE_USERNAME"], ENV["AIRBRAKE_AUTH_TOKEN"], ENV["AIRBRAKE_PROJECT_ID"])
-    # @pivotal = Pivotal.new(ENV["AIRBRAKE_REQUESTOR"])
-  # end
-  
   use Rack::Auth::Basic do |username, password|
     username == ENV["HTTP_BASIC_USERNAME"] && password == ENV["HTTP_BASIC_PASSWORD"]
   end
